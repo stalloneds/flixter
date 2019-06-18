422b34b4707e92a23c23c0a86edbb77a03264d53
Stallone Dias <stalloneds@gmail.com>
2019-06-18 01:31:41 +0000

    image uploading is working

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,7 +5,13 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   # storage :file
+  # storage :aws
+
+  if Rails.env.production?
   storage :aws
+else
+  storage :file
+end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
