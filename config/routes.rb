60618e7bcfdb99091b6778ee0d388198936bff47
Stallone Dias <stalloneds@gmail.com>
2019-06-26 20:57:31 +0000

    bug when trying to add new lesson

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
     # resources :sections, only: [:update]
     resources :lessons, only: [:update]
     resources :sections, only: [:update] do
-      resources :lessons, only: [:new, :create]
+      resources :lessons, only: [:new]
     end
     resources :courses, only: [:new, :create, :show] do
       resources :sections, only: [:create]
